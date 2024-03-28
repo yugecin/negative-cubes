52fc2d88dccf81f7798bb1d700a966f8d1c72fa9
yugecin <yugecin@users.noreply.github.com>
2024-03-28 18:15:45 +0100

    second phase cube

--- frag.glsl
+++ frag.glsl
@@ -7,6 +7,9 @@ layout (location=0) uniform vec4 fpar[2];
 layout (location=2) uniform vec4 debug[2]; //noexport
 #define MAT_GROUND 0
 #define MAT_BLU 1
+#define MAT_WHI 2
+#define MAT_WHA 3
+#define MAT_WHE 4
 int i;
 vec3 gHitPosition = vec3(0);
 float PI = acos(-1.);
@@ -15,6 +18,26 @@ float diag = sqrt(2.);
 mat2 rot2(float a){float s=sin(a),c=cos(a);return mat2(c,s,-s,c);}
 
 vec2 m(vec2 b, vec2 a){return a.x<b.x?a:b;}
+
+bool n = true;
+vec3 BLU = vec3(.2,.2,.8);
+
+vec2 neg(vec3 p)
+{
+	p.xy += 1.;
+	p.z -= iTime;
+	p.xy = mod(p.xy, 2.) - 1.;
+	p.yz *= rot2(iTime);
+	float box = length(max(abs(p)-vec3(.5),0.));
+	float right = max(box, -dot(p-vec3(0.,0.,-.5),normalize(vec3(1.,1.,-1.))));
+	vec2 r = vec2(right, MAT_WHI);
+	float mid = max(box, -dot(p-vec3(0.,0.,-.5),normalize(vec3(-1.,-1.,1.))));
+	vec3 schel = vec3(-.499,0.,0.);
+	float left = max(mid, -dot(p-schel,normalize(vec3(-1.,0.,0.))));
+	mid = max(mid, -dot(p-schel,normalize(vec3(1.,0.,0.))));
+	r = m(r, vec2(mid, MAT_WHA));
+	return m(r, vec2(left, MAT_WHE));
+}
 
 vec2 blu(vec3 p)
 {
@@ -35,8 +58,7 @@ vec2 map(vec3 p)
 
 	//p.yz *= rot2(3.1415/4.);
 
-	vec2 r = blu(p);
-	return r;
+	return n ? neg(p) : blu(p);
 }
 
 vec3 norm(vec3 p, float dist_to_p)
@@ -69,8 +91,10 @@ vec3 getmat(vec4 r)
 {
 	vec3 p = gHitPosition.xyz;
 	switch (int(r.w)) {
-	case MAT_BLU:
-		return vec3(.2,.2,.8);
+	case MAT_BLU: return BLU;
+	case MAT_WHI: return vec3(.7);
+	case MAT_WHA: return vec3(1.,0.,0.);
+	case MAT_WHE: return vec3(0.,1.,1.);
 	}
 	return vec3(1.);
 }
@@ -129,7 +153,7 @@ void main()
 			uv.y /= iResolution.x/iResolution.y;
 #endif //noexport
 			//vec3 rd = rdbase*normalize(vec3(uv,1))
-			vec3 col = vec3(.8);
+			vec3 col = n ? BLU : vec3(.8);
 			ro = vec3(uv*5.,-90.), rd=vec3(0.,0.,1.);
 
 			vec4 result = march(ro, rd);
