473343b35c87cf45383a7ae96b039d451d3edbf4
yugecin <yugecin@users.noreply.github.com>
2024-03-28 19:29:23 +0100

    switch over automatically and adjust colors

--- frag.glsl
+++ frag.glsl
@@ -7,7 +7,7 @@ layout (location=0) uniform vec4 fpar[2];
 layout (location=2) uniform vec4 debug[2]; //noexport
 #define MAT_GROUND 0
 #define MAT_BLU 1
-#define MAT_WHI 2
+#define MAT_RIG 2
 #define MAT_WHA 3
 #define MAT_WHE 4
 int i;
@@ -15,22 +15,25 @@ vec3 gHitPosition = vec3(0);
 float PI = acos(-1.);
 float diag = sqrt(2.);
 
+#define SIZE .5
+
 mat2 rot2(float a){float s=sin(a),c=cos(a);return mat2(c,s,-s,c);}
 
 vec2 m(vec2 b, vec2 a){return a.x<b.x?a:b;}
 
-bool n = true;
+bool n = false;
 vec3 BLU = vec3(.2,.2,.8);
 
-vec2 neg(vec3 p)
+vec2 neg(vec3 p, float tt)
 {
-	p.xy += 1.;
-	p.z -= iTime;
+	//p.xy += 1.;
+	p.x += SIZE * 4.;
+	p.z -= tt;
 	p.xy = mod(p.xy, 2.) - 1.;
-	p.yz *= rot2(iTime);
+	p.yz *= rot2(tt);
 	float box = length(max(abs(p)-vec3(.5),0.));
 	float right = max(box, -dot(p-vec3(0.,0.,-.5),normalize(vec3(1.,1.,-1.))));
-	vec2 r = vec2(right, MAT_WHI);
+	vec2 r = vec2(right, MAT_RIG);
 	float mid = max(box, -dot(p-vec3(0.,0.,-.5),normalize(vec3(-1.,-1.,1.))));
 	vec3 schel = vec3(-.499,0.,0.);
 	float left = max(mid, -dot(p-schel,normalize(vec3(-1.,0.,0.))));
@@ -39,12 +42,12 @@ vec2 neg(vec3 p)
 	return m(r, vec2(left, MAT_WHE));
 }
 
-vec2 blu(vec3 p)
+vec2 blu(vec3 p, float tt)
 {
-	p.x += iTime;
+	p.x += tt;
 	p.xy = mod(p.xy, 2.) - 1.;
-	//p.xz *= rot2(iTime);
-	p.xy *= rot2(iTime);
+	//p.xz *= rot2(tt);
+	p.xy *= rot2(tt);
 	return vec2(length(max(abs(p)-vec3(.5),0.)), MAT_BLU);
 }
 
@@ -58,7 +61,8 @@ vec2 map(vec3 p)
 
 	//p.yz *= rot2(3.1415/4.);
 
-	return n ? neg(p) : blu(p);
+	float tt = mod(iTime, 4.*PI);
+	return tt > PI ? neg(p, tt - PI) : blu(p, tt);
 }
 
 vec3 norm(vec3 p, float dist_to_p)
@@ -92,7 +96,7 @@ vec3 getmat(vec4 r)
 	vec3 p = gHitPosition.xyz;
 	switch (int(r.w)) {
 	case MAT_BLU: return BLU;
-	case MAT_WHI: return vec3(.7);
+	case MAT_RIG: return vec3(66.,66.,123.)/255.;
 	case MAT_WHA: return vec3(1.,0.,0.);
 	case MAT_WHE: return vec3(0.,1.,1.);
 	}
@@ -101,6 +105,8 @@ vec3 getmat(vec4 r)
 
 vec3 colorHit(vec4 result, vec3 rd, vec3 normal, vec3 mat)
 {
+	float tt = mod(iTime, 4.*PI);
+	if (tt > PI) return mat;
 	vec3 lig = normalize(vec3(.3,.3,-1.));
 	return mat * 4. * clamp(dot(normal, lig), .0, 1.);
 	/*
@@ -169,5 +175,7 @@ void main()
 	resultcol /= 4.;
 #endif
 
-	c = vec4(pow(resultcol, vec3(.4545)), 1.0); // pow for gamma correction because all the cool kids do it
+	float tt = mod(iTime, 4.*PI);
+	if (tt < PI) resultcol = pow(resultcol, vec3(.4545));
+	c = vec4(resultcol, 1.0);
 }
